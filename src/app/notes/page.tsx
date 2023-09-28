ecd29425d7ead680cc54761574adde988898a7b6
KENNY GU <kgu@college.harvard.edu>
2023-09-28 02:27:53 -0400

    long overdue update

--- src/app/notes/page.tsx
+++ src/app/notes/page.tsx
@@ -20,6 +20,71 @@ export default function Home() {
         </p>
 
         <p className="lg:w-50">
+        <span className="font-bold">Wednesday, Sept 27:&nbsp;</span>
+          From Stat 286: learned about <Link href="https://www.jstor.org/stable/25054228" className="accented hover:underline">Wantchekon (2003)</Link>, which analyzes a experiment in the 2001 Benin presidential election. Essentially, the experiment involved randomizing campaign strategies by village, finding that clientalism was a winning strategy in the election. The punch line for 286 is that accounting for clustering and stratifcation in SE calculation matters.
+        </p>
+
+        <p className="lg:w-50">
+        <span className="font-bold">Tuesday, Sept 26:&nbsp;</span>
+          Talked about random features models, where the kernel is some function averaged over a weight distribution. For an appropriate function choice, we get something that appears to be a neural network with random weights. There&apos;s also a connection to the idea that infinite width neural networks are equivalent to Gaussian processes.
+        </p>
+
+        <p className="lg:w-50">
+        <span className="font-bold">Monday, Sept 25:&nbsp;</span>
+          From Stat 211: learned about the asymptotic Cramer-Rao lower bound. Under some conditions, if an estimator is asymptotically unbiased (the normalized difference converges to a centered Gaussian), then the variance of that Gaussian can be bounded by roughly the same bound from the standard CRLB.
+        </p>
+
+        <p className="lg:w-50">
+        <span className="font-bold">Sunday, Sept 24:&nbsp;</span>
+          Learned that, given limited local memory, we can use outer products of parts of the rows/columns of two matrices to compute blocks of a matrix. There&apos;s a clever way to choose the order of computations to maximize arithmetic intensity.
+        </p>
+
+        <p className="lg:w-50">
+        <span className="font-bold">Saturday, Sept 23:&nbsp;</span>
+          Was a little concerned about an apple that had been sitting out for a while and was tinted red inside, but I learned that some apples can be red inside because of the <Link href="https://www.salon.com/2021/11/25/red-fleshed-apples-the-science-behind-an-uncommon-and-much-desired-apple-breed/" className="accented hover:underline">presence of anthocyanins</Link>. Hopefully that&apos;s the red color and not some kind of poison...
+        </p>
+
+        <p className="lg:w-50">
+        <span className="font-bold">Friday, Sept 22:&nbsp;</span>
+          Learned that Harvard Stadium is the &quot;nation&apos;s oldest permanent concrete structure dedicated to intercollegiate athletics&quot;, according to <Link href="https://en.wikipedia.org/wiki/Harvard_Stadium" className="accented hover:underline">Wikipedia</Link>.
+        </p>
+
+        <p className="lg:w-50">
+        <span className="font-bold">Thursday, Sept 21:&nbsp;</span>
+          Continued discussing Chow-Liu for Ising models in 6.S896. We&apos;ve seen in the past the <InlineMath math="N = O(n \log n / \epsilon^2)"/> samples is sufficient to learn the Ising models in TV to <InlineMath math="\epsilon)"/>. Koehler shows that this is also necessary.
+        </p>
+        
+        <p className="lg:w-50">
+        <span className="font-bold">Wednesday, Sept 20:&nbsp;</span>
+          Learned about the sum-of-squares method in CS 224. Roughly, the method involves taking an inefficient algorithm with a simple-ish proof of correctness and maximizing over pseudo-distributions to get a more efficient algorithm.
+        </p>
+
+        <p className="lg:w-50">
+        <span className="font-bold">Tuesday, Sept 19:&nbsp;</span>
+          From 6.791: implicit regularization gives us a way of thinking of gradient descent as a regularizer. There&apos;s a really neat way of rewriting the weights from gradient descent and the weights from ridge regression using SVD; effectively, both gradient descent and ridge regression (for earlier iterations and larger regularization terms) filter out smallest singular values, thus avoiding instability.
+        </p>
+
+        <p className="lg:w-50">
+        <span className="font-bold">Monday, Sept 18:&nbsp;</span>
+          From CS 242: there are specialized AI accelerator chips used to perform efficient NN structured pruning. N:M sparsity accelerators, like NVIDIA&apos;s A100 Sparse Tensor Core, keep at most N nonzero weights per every M continuous weights.
+        </p>
+
+        <p className="lg:w-50">
+        <span className="font-bold">Sunday, Sept 17:&nbsp;</span>
+          Learned that sprained ankles can take anywhere from two weeks to six months to heal well. Hoping to get back to running soon!
+        </p>
+
+        <p className="lg:w-50">
+        <span className="font-bold">Saturday, Sept 16:&nbsp;</span>
+          Learned about surrogate outcomes from Stat 286 problem set: in a lot of applications, waiting for long-term outcomes is cumbersome or infeasible; instead, the average treatment effect on some surrogate outcome variable is the same as the ATE of interest if the long-term outcome is independent of the treatment given the surrogate outcome.
+        </p>
+
+        <p className="lg:w-50">
+        <span className="font-bold">Friday, Sept 15:&nbsp;</span>
+          From helping a friend with a problem: consider the Fibonacci-like sequence that begins with <InlineMath math="1, \varphi, \dots" />. Then, the ratio between consecutive terms is <InlineMath math="\varphi"/>, the golden ratio.
+        </p>
+
+        <p className="lg:w-50">
         <span className="font-bold">Thursday, Sept 14:&nbsp;</span>
           Learned about undirected graphical models in 6.S896 today. Consider an undirected graph <InlineMath math="G = (V,E)"/>. Associating vertices with random variables, consider a density <InlineMath math="p(x_V)"/> such that edges exist between vertices <InlineMath math="(i,j)"/> for <InlineMath math="i \neq j" /> if and only if the corresponding r.v.s are conditionally dependent given the other r.v.s. <Link href="https://en.wikipedia.org/wiki/Hammersley–Clifford_theorem" className="accented hover:underline">Hammersley-Clifford (1971)</Link> proves that if the density is strictly positive for all possible values of <InlineMath math="x_V"/>, the previous property implies that we can factor <InlineMath math="p"/> into clique potentials (functions depending only on each clique).
         </p>
