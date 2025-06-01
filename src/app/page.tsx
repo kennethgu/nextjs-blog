e98b209cd28931e88b0f385038155a8ba7918bc1
Kenny Gu <39811362+kennethgu@users.noreply.github.com>
2025-06-01 12:04:39 -0400

    grad update

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -10,8 +10,11 @@ export default function Home() {
       <div className="py-1 px-5 shrink">
         <img src="/hs.jpg" className="float-right lg:w-1/4 w-2/5 ml-3.5 rounded-md" />
         <p className="text-justify">
-        Hello! My name&apos;s Kenny, and I&apos;m a senior at Harvard College in <Link href="https://currier.harvard.edu" className="accented">Currier House</Link> studying statistics, math, and computer science. My research interests revolve (broadly) around high-dimensional statistics and machine learning: I&apos;m interested in developing performance guarantees for and improving the efficiency of statistical and ML methods in high-dimensional settings.
-        At Harvard, I&apos;ve been fortunate to be advised by <Link href="https://sites.harvard.edu/prs499/" className="accented">Prof. Pragya Sur</Link> and <Link href="https://sites.google.com/view/morganeaustern/home" className="accented">Prof. Morgane Austern</Link>.
+        Hello! My name&apos;s Kenny, and I&apos;m a recent graduate of Harvard College, where I lived in <Link href="https://currier.harvard.edu" className="accented">Currier House</Link> and studied statistics, math, and computer science. My research interests revolve (broadly) around high-dimensional statistics and machine learning: I&apos;m interested in developing performance guarantees for and improving the efficiency of statistical and ML methods in high-dimensional settings.
+        At Harvard, I&apos;ve been fortunate to be advised by <Link href="https://sites.harvard.edu/prs499/" className="accented">Prof. Pragya Sur</Link> and <Link href="https://sites.google.com/view/morganeaustern/home" className="accented">Prof. Morgane Austern</Link>. 
+        </p>
+        <p className="text-justify">
+        This coming fall, I will begin my PhD in Statistics at <Link href="https://statistics.stanford.edu" className="accented">Stanford University</Link>.
         </p>
         <p className="text-justify">
         Outside of research, I also love thinking and talking about electoral politics/strategies (especially in my lovely home state of Michigan), education policy, pop culture, AI ethics, and the intersection(s) of these things! 
@@ -19,9 +22,6 @@ export default function Home() {
         <p>
           email: <span className="italic">kgu [at] college [dot] harvard [dot] edu</span>
         </p>
-        {/* <p>
-          I&apos;m mostly creating this website/blog for myself (or so I tell myself): to keep track of things I&apos;m learning and maybe jot down some words on things I&apos;m thinking about. Not sure how often I&apos;ll update this, but I&apos;ll try to keep it somewhat regular.
-        </p> */}
       </div>
     </main>
   )
