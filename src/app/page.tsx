b492f130cf6d028633de49c4c2b49e0eff1f380c
Kenny Gu <39811362+kennethgu@users.noreply.github.com>
2025-11-15 16:38:24 -0800

    nov 2025 update

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -10,17 +10,18 @@ export default function Home() {
       <div className="py-1 px-5 shrink">
         <img src="/hs.jpg" className="float-right lg:w-1/4 w-2/5 ml-3.5 rounded-md" />
         <p className="text-justify">
-        Hello! My name&apos;s Kenny, and I&apos;m a recent graduate of Harvard College, where I lived in <Link href="https://currier.harvard.edu" className="accented">Currier House</Link> and studied statistics, math, and computer science. My research interests revolve (broadly) around high-dimensional statistics and machine learning: I&apos;m interested in developing performance guarantees for and improving the efficiency of statistical and ML methods in high-dimensional settings.
-        At Harvard, I&apos;ve been fortunate to be advised by <Link href="https://sites.harvard.edu/prs499/" className="accented">Prof. Pragya Sur</Link> and <Link href="https://sites.google.com/view/morganeaustern/home" className="accented">Prof. Morgane Austern</Link>. 
+        I am a first-year statistics PhD student at <Link href="https://statistics.stanford.edu" className="accented">Stanford</Link>. My research interests revolve (broadly) around high-dimensional statistics and machine learning: I&apos;m interested in developing performance guarantees for and improving the efficiency of statistical and ML methods in high-dimensional settings.
         </p>
         <p className="text-justify">
-        This coming fall, I will begin my PhD in Statistics at <Link href="https://statistics.stanford.edu" className="accented">Stanford University</Link>.
+        In 2025, I graduated from Harvard College, where I lived in <Link href="https://currier.harvard.edu" className="accented">Currier House</Link> and studied statistics, math, and computer science. At Harvard, I was fortunate to be advised by <Link href="https://sites.harvard.edu/prs499/" className="accented">Prof. Pragya Sur</Link> and <Link href="https://sites.google.com/view/morganeaustern/home" className="accented">Prof. Morgane Austern</Link>. 
         </p>
         <p className="text-justify">
         Outside of research, I also love thinking and talking about electoral politics/strategies (especially in my lovely home state of Michigan), education policy, pop culture, AI ethics, and the intersection(s) of these things! 
         </p>
         <p>
-          email: <span className="italic">kgu [at] college [dot] harvard [dot] edu</span>
+          email: <span className="italic">kennygu [at] stanford [dot] edu</span>
+          <br />
+          last updated: <span className="italic">Nov 2025</span>
         </p>
       </div>
     </main>
